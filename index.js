0b9ad4c5bfde266de0cea1e90b62955975c174c9
Spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2020-08-30 19:11:39 +0200

    added JS todo

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ let smoothScrollArray = [["services-scroll-target"]]
 let heroScroll = document.getElementById('heroScroll')
 heroScroll.addEventListener('click', () => services.scrollIntoView({behavior: 'smooth'}))
 
+// TODO Add smooth scroll to the navlinks
 let navLinks = document.getElementsByClassName("nav-links")
 for(let link of navLinks) {
+    // elementId = link.href.split(#) get element [-1]
+    // let element = document.getElementById(elementId)
+    // add event listener click
     console.log(link.href);
 }
